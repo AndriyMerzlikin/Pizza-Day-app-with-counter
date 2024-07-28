ab14806654d258a18d0261958a00c8c3f2ebcd71
AndriyMerzlikin <lastheroofme@gmail.com>
2024-07-28 11:10:34 +0300

    fixed form button

--- assets/NotFoundPage-DGeCnQnA.js
+++
@@ -1,1 +0,0 @@
-import{j as o,L as t}from"./index-4PgAdi-P.js";const s=()=>o.jsxs("h1",{className:"not-found",children:["Something went wrong. Please, go this ",o.jsx(t,{to:"/",children:"LINK"})," to go Home!"]});export{s as default};
